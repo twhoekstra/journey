45f3d721035a4a8340f4191f27d5ceacbb6d582d
Thijn Hoekstra <twhoekstra@student.eur.nl>
2020-07-19 16:33:38 +0200

    3

---
+++ journey/Models/Pathogen.swift
@@ -0,0 +1,25 @@
+//
+//  Pathogen.swift
+//  journey
+//
+//  Created by Manon Hoekstra on 10/06/2020.
+//  Copyright © 2020 Thijn Hoekstra. All rights reserved.
+//
+
+import SwiftUI
+import CoreLocation
+
+struct Pathogen: Hashable, Codable, Identifiable {
+    var id: Int
+    var name: String
+    var aka: String
+    var lethality: Double
+    var description: String
+    fileprivate var imageName: String
+}
+
+extension Pathogen {
+    var image: Image {
+        ImageStore.shared.image(name: imageName)
+    }
+}
